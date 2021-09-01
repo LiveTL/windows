813ab3636d74e7b0f6f1638a24734595d00e854c
Aaron Thapa <notsmart98@gmail.com>
2021-08-31 22:07:04 -0400

    add more mchad things

--- mchad.py
+++ mchad.py
@@ -2,5 +2,15 @@ import requests
 
 MCHAD = 'https://repo.mchatx.org'
 
-def getRoom(id):
-    print(requests.get(MCHAD + '/Room/?link=YT_' + id).json())
+def getRoomList():
+    return requests.get(f'{MCHAD}/Room/').json()
+
+def getRoom(id, idonly = True):
+    if idonly:
+        id += 'YT_'
+    return requests.get(f'{MCHAD}/Room/?link={id}').json()
+
+if __name__ == "__main__":
+    print('mchad test')
+    print(getRoomList())
+    print(getRoom('mJwpVT1WvLg'))
