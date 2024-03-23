75c5452f8ff04418acbc948abb9a204ea5979d37
Aaron Thapa <notsmart98@gmail.com>
2024-03-23 22:08:00 +0545

    nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in with pkgs; rec {
+        devShell = mkShell rec {
+          packages = [
+            imagemagick
+          ];
+          buildInputs = [
+            libxkbcommon
+            libGL
+
+            # WINIT_UNIX_BACKEND=wayland
+            wayland
+
+            # WINIT_UNIX_BACKEND=x11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      });
+}
